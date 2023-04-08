a840d135f40f47b0a56cc46cc83090ef8fe367bf
aniskhra <annisakhoiria10@gmail.com>
2023-04-08 10:24:25 +0700

    praktikum modul 8

---
+++ MODUL_8/latihan5.3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Loop for</title>
+</head>
+<body>
+
+<?php
+
+for ($i = 0; $i < 10; $i++) { 
+    echo $i;
+}
+
+?>
+
+</body>
+</html>
---
+++ MODUL_8/latihan6.1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Definisi Fungsi/Prosedur</title>
+</head>
+<body>
+
+<?php
+// Contoh prosedur
+function do_print() {
+    // Mencetak informasi timestamp
+    echo time();
+}
+
+// Memanggil prosedur
+do_print(); 
+
+echo '<br />';
+
+// Contoh fungsi penjumlahan
+function jumlah($a, $b) { 
+    return ($a + $b);
+}
+
+echo jumlah(2, 3);
+// Output: 5
+?>
+
+</body>
+</html>
