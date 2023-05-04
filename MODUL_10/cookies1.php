b5fb6d56341a85cf54e681101002e2ba51a4da8b
aniskhra <annisakhoiria10@gmail.com>
2023-05-04 07:58:34 +0700

    merapikan comment

--- MODUL_10/cookies1.php
+++ MODUL_10/cookies1.php
@@ -1,8 +1,14 @@
 <?php
-    $value1 = 'anis'; // nilai variabel $value1 di-set sebagai 'anis'
-    $value2 = 'Annisa Alimatul Khoiria'; // nilai variabel $value2 di-set sebagai 'Annisa Alimatul Khoiria'
-    setcookie("username", $value1); // cookie dengan nama "username" dan nilai $value1 diset
-    setcookie("nama_lengkap", $value2, time()+3600); // cookie dengan nama "nama_lengkap", nilai $value2, dan loginnya expired 3600 detik (1 jam)
-    echo "<h2>Ini halaman untuk set cookie</h2>"; // menampilkan judul halaman
-    echo "<h2>Klik <a href='cookies2.php'>disini </a> untuk mengecek cookies.</h2>"; // menampilkan hyperlink untuk menuju halaman "cookies2.php" yang berfungsi untuk mengecek cookie
+    // nilai variabel $value1 di-set sebagai 'anis'
+    $value1 = 'anis'; 
+    // nilai variabel $value2 di-set sebagai 'Annisa Alimatul Khoiria'
+    $value2 = 'Annisa Alimatul Khoiria'; 
+    // cookie dengan nama "username" dan nilai $value1 diset
+    setcookie("username", $value1); 
+    // cookie dengan nama "nama_lengkap", nilai $value2, dan loginnya expired 3600 detik (1 jam)
+    setcookie("nama_lengkap", $value2, time()+3600); 
+    // menampilkan judul halaman
+    echo "<h2>Ini halaman untuk set cookie</h2>"; 
+    // menampilkan hyperlink untuk menuju halaman "cookies2.php" yang berfungsi untuk mengecek cookie
+    echo "<h2>Klik <a href='cookies2.php'>disini </a> untuk mengecek cookies.</h2>"; 
 ?>
--- MODUL_10/session3.php
+++ MODUL_10/session3.php
@@ -1,12 +1,14 @@
 <?php
     session_start(); // memulai session
-
-    if (isset($_SESSION['login'])) { // cek apakah user telah login sebelumnya
-        unset ($_SESSION); // menghapus semua variabel yang terdapat dalam session
+    // cek apakah user telah login sebelumnya
+    if (isset($_SESSION['login'])) { 
+        // menghapus semua variabel yang terdapat dalam session
+        unset ($_SESSION); 
         session_destroy(); // menghapus semua data session
-        echo "<h1>Kamu sudah berhasil logout/keluar</h1>"; // menampilkan pesan berhasil logout
+        // menampilkan pesan berhasil logout
+        echo "<h1>Kamu sudah berhasil logout/keluar</h1>";
+        // menampilkan pesan untuk login kembali dan tidak bisa mengakses halaman home lagi
         echo "<h1>Klik <a href='session1.php'/>disini</a> untuk login lagi.<br>Kamu tidak bisa masuk ke
-            <a href='session2.php'>HOME (beranda)</a>lagi.</h2>"; // menampilkan pesan untuk login kembali dan tidak bisa mengakses halaman home lagi
+            <a href='session2.php'>HOME (beranda)</a>lagi.</h2>"; 
     }
-    
 ?>
